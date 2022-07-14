4ce4d5ce98d3712f6ebf4677018023218601d74a
Ameya Rao <ameyarao98@gmail.com>
2022-07-14 21:10:06 +0100

    temp

--- main.go
+++ main.go
@@ -6,25 +6,37 @@ import (
 	"log"
 	"os"
 
+	"github.com/ameyarao98/pong-bot/contract"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	kovanEndpoint := os.Getenv("KOVAN_ENDPOINT")
-	cl, err := ethclient.Dial(kovanEndpoint)
+	client, err := ethclient.Dial(kovanEndpoint)
 	if err != nil {
 		log.Fatal(err)
 	}
-	block, err := cl.BlockByNumber(context.Background(), nil)
+	block, err := client.BlockByNumber(context.Background(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("block number %v", block.Number())
-	// channel := make(chan *contract.ContractPing)
-	// go func() {
-	// 	sub, err := ctr.WatchDeposited(&bind.WatchOpts{Context: context.Background(), Start: nil}, channel)
-	// 	defer sub.Unsubscribe()
-	// }()
-	// event := <-channel
+	fmt.Printf("block number %v\n", block.Number())
+
+	ctr, err := contract.NewContract(common.HexToAddress(os.Getenv("PING_PONG_CONTRACT_ADDRESS")), client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	channel := make(chan *contract.ContractPing)
+	go func() {
+		sub, err := ctr.WatchPing(&bind.WatchOpts{Context: context.Background(), Start: nil}, channel)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer sub.Unsubscribe()
+	}()
+	fmt.Println(<-channel)
 }

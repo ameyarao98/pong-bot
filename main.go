b1422210010e12dfb1feac9da02136c6264a7911
Ameya Rao <ameyarao98@gmail.com>
2022-07-14 16:55:54 +0100

    basic block number example

--- main.go
+++ main.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/chenzhijie/go-web3"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	kovanAPIKey := os.Getenv("KOVAN_API_KEY")
-	fmt.Println("https://kovan.infura.io/v3/" + kovanAPIKey)
+	web3, err := web3.NewWeb3("https://kovan.infura.io/v3/" + kovanAPIKey)
+	if err != nil {
+		panic(err)
+	}
+	blockNumber, err := web3.Eth.GetBlockNumber()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Current block number: ", blockNumber)
 }
